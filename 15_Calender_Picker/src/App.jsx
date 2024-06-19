8ac268545587a2f849a8a56ebefc2468022d2c16
pradipkaityuiux <pradipforeducation@gmail.com>
2024-06-19 20:04:42 +0530

    Date Picker Calendar Project

---
+++ 15_Calender_Picker/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Calendar from './component/Calender'
+
+function App() {
+  return (
+    <>
+      <Calendar month={10} year={2024}/>
+    </>
+  )
+}
+
+export default App
